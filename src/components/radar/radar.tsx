d1df2728d9c29eb80a2a50a6240bd0b24fdc66e3
alfaiv <alfaiv@yandex.ru>
2024-11-23 15:57:53 +0300

    fix mini table on state

--- src/components/radar/radar.tsx
+++ src/components/radar/radar.tsx
@@ -1,34 +1,69 @@
 import style from "./radar.module.scss";
 import Plot from "react-plotly.js";
-import Button from "@components/button/button";
+// import Button from "@components/button/button";
 import plotConfig from "./plot.config";
 import TableSatellite from "@components/table/table";
+import { FC } from "react";
+import {
+  Grid2,
+  Button,
+  Typography,
+  Stack,
+  Alert,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
-const Radar = () => {
+const Radar: FC = () => {
   return (
     <div className={style.radar}>
       <div className={style.radar__plot}>
         <Plot data={plotConfig.data} layout={plotConfig.layout} />
       </div>
       <div className={style.radar__table}>
-        <div className={style.radar__table__status}>
-          <p>Статус калибровки:</p>
-          <div>Калиброван</div>
-        </div>
-        <Button text="Проверить" />
-        <div className={style.radar__table__task}>
-          <p>Выполняется задание:</p>
-          <div className={style.radar__table__task_p}>
-            <p>Спутник: </p>
-            <p>Начало записи: </p>
-            <p>Окончание записи: </p>
-          </div>
-          <Button text="Прервать задание" />
-        </div>
+        <Stack
+          spacing={2}
+          direction="row"
+          alignItems={"center"}
+          alignContent={"center"}
+        >
+          <Typography component={"p"} variant="body1">
+            Статус калибровки:
+          </Typography>
+          <Alert sx={{ maxWidth: "200px" }} severity="success">
+            Калиброван
+          </Alert>
+          {/* <Alert sx={{ maxWidth: "200px"}} severity="warning">Нет данных</Alert> */}
+          {/* <Alert sx={{ maxWidth: "200px"}} severity="error">Калибровка отсутствует</Alert> */}
+          <Button variant="contained">Провести калибровку</Button>
+        </Stack>
+        <TableContainer >
+          <Table sx={{border: "2px solid white", borderRadius: "15px"}}>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <TableRow sx={{ mb: 2 }}>Выполняется задание: {"Нет данных"} </TableRow>
+                  <TableRow sx={{ mb: 2 }}>
+                    <Button variant="contained">Прервать задание</Button>
+                  </TableRow>
+                </TableCell>
+                <TableCell >
+                  <TableRow sx={{ mb: 10 }}>Спутник: {"Нет данных"} </TableRow>
+                  <TableRow sx={{ mb: 10 }}>Начало записи: {"Нет данных"} </TableRow>
+                  <TableRow sx={{ mb: 10 }}>Окончание записи: {"Нет данных"}</TableRow>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
         <TableSatellite />
-      </div>  
+      </div>
     </div>
   );
 };
 export default Radar;
-
