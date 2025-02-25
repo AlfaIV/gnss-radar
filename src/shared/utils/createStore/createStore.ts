a9918fb9f343085874621ee2b3cdb9a8caa73586
Glibusss <expansifff@gmail.com>
2025-02-25 20:50:39 +0300

    fix

--- src/shared/utils/createStore/createStore.ts
+++ src/shared/utils/createStore/createStore.ts
@@ -1,6 +1,6 @@
 import { createWithEqualityFn } from 'zustand/traditional'
 import { devtools, subscribeWithSelector } from 'zustand/middleware'
-import type { StateCreator, StoreApi } from 'zustand'
+import type { StateCreator } from 'zustand'
 
 type StoreFunction<S> = StateCreator<
   S,
@@ -9,21 +9,28 @@ type StoreFunction<S> = StateCreator<
   S
 >
 
-interface CreateStoreOptions {
+interface CreateStoreOptions<S> {
   name?: string
+  partialize?: (state: S) => Partial<S>
 }
 
-const equalityFn = <T>(a: T, b: T): boolean => JSON.stringify(a) === JSON.stringify(b)
+const defaultEqualityFn = <T>(a: T, b: T): boolean =>
+  JSON.stringify(a) === JSON.stringify(b)
 
-function createStore<S>(fn: StoreFunction<S>, options?: CreateStoreOptions) {
-  const middleware = (f: StoreFunction<S>) => 
+function createStore<S>(fn: StoreFunction<S>, options?: CreateStoreOptions<S>) {
+  const middleware = (f: StoreFunction<S>) =>
     devtools(subscribeWithSelector(f), { name: options?.name })
 
+  const equalityFnGeneric = options?.partialize
+    ? <T>(a: T, b: T): boolean =>
+        JSON.stringify(options.partialize!(a as unknown as S)) === JSON.stringify(options.partialize!(b as unknown as S))
+    : defaultEqualityFn
+
   if (process.env.NODE_ENV === 'development') {
-    return createWithEqualityFn(middleware(fn), equalityFn)
+    return createWithEqualityFn(middleware(fn), equalityFnGeneric)
   }
   
-  return createWithEqualityFn(fn, equalityFn)
+  return createWithEqualityFn(fn, equalityFnGeneric)
 }
 
 export default createStore
